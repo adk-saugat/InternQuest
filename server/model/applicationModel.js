537747db3acf1b222c280d7f06ef0f8cf2dbded7
adk-saugat <adhikarisaugat34@gmail.com>
2025-03-07 00:17:46 -0600

    starting the application router and model

---
+++ server/model/applicationModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose"
+
+const applicationSchema = new mongoose.Schema({
+  companyName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  position: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Applied", "Accepted", "Rejected", "In Progress"],
+    default: "Applied",
+    trim: true,
+  },
+  link: {
+    type: String,
+    trim: true,
+  },
+  notes: {
+    type: String,
+    trim: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+})
+
+const Application = mongoose.model("Application", applicationSchema)
+
+export { Application }
