6ce503bb6f55ddf0758c9252e8e81c12a81b7c59
adk-saugat <adhikarisaugat34@gmail.com>
2025-03-31 20:01:40 -0500

    application delete endpoint added

--- server/routes/applicationRouter.js
+++ server/routes/applicationRouter.js
@@ -17,12 +17,30 @@ appRouter.post("/create", auth, async (req, res) => {
   }
 })
 
+// Get all Applications
 appRouter.get("/all", auth, async (req, res) => {
   try {
     const applications = await Application.find({
       token: req.header["auth-token"],
     })
     res.status(200).send(applications)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+})
+
+// Delete application
+appRouter.delete("/:id", auth, async (req, res) => {
+  try {
+    const application = await Application.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id,
+    })
+    if (!application) {
+      return res.status(404).send({ error: "Application to delete not Found!" })
+    }
+
+    res.status(200).send(application)
   } catch (error) {
     res.status(400).send(error)
   }
